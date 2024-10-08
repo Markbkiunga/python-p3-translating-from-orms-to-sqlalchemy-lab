375139121fb0ac2f1cf246b1fadbbcb60cd697fa
Mark Brian <markbkiunga2017@gmail.com>
2024-10-08 12:19:20 +0300

    Completes Lab

--- lib/models.py
+++ lib/models.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class Dog(Base):
-    __tablename__ = 'dogs'
+    __tablename__ = "dogs"
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     breed = Column(String())
+
+
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///:memory:")
+    Base.metadata.create_all(engine)
